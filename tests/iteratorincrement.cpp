95bfe30eed004b46407fc273f2ef7a024a4ab459
Satya Gowtham Kudupudi <satyagowtham.k@gmail.com>
2016-10-26 12:19:06 +0530

    Adding forgotten files

---
+++ tests/iteratorincrement.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<map>
+#include<vector>
+#include<base/mystdlib.h>
+#include<FFJSON.h>
+using namespace std;
+
+int child_exit_status=0;
+struct MyStruct{
+    template<typename T>
+    operator T&() {
+        cout<<"size: "<<sizeof(T)<<endl;
+        if (m_iSize == sizeof(T)) {
+            return *reinterpret_cast<T*> (m_pV);
+        }
+        return *reinterpret_cast<T*> (NULL);
+    }
+    MyStruct(void* pV,int iSize){m_pV=pV;m_iSize=iSize;}
+    void* m_pV=NULL;
+    int m_iSize=0;
+};
+
+int main(int argc, char** argv){
+    map<string, int> mStrInt;
+    vector<map<string, int>::iterator > vStrIntIt;
+    mStrInt["gowtham"]=100;
+    mStrInt["satya"]=200;
+    map<string,int>::iterator itmStrInt= mStrInt.begin();
+    vStrIntIt.push_back(++itmStrInt);
+    vStrIntIt.push_back(mStrInt.begin());
+    vector<map<string,int>::iterator>::iterator ititmStrInt=vStrIntIt.begin();
+    ititmStrInt++;
+    cout<<(*ititmStrInt)->first<<endl;
+    MyStruct mystruct((void*)new FFJSON(),sizeof(FFJSON));
+    FFJSON& i=(FFJSON&)mystruct;
+    cout<<i<<endl;
+}
